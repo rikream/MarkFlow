ef58049e8afc9172653ace17792b655216679cb9
Rikim Rana <rikimrana1@gmail.com>
2026-08-01 22:36:22 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,9 @@ app = FastAPI()# app object this is the core obj for accesing all endpoints
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://mark-flow-gamma.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://markflow-converter.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
